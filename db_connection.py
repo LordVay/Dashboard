1f6ee157f4445df7878fb274fa5309d11e1b57ce
LordVay <vaylordb@gmail.com>
2026-02-18 21:08:59 +0800

    model

--- db_connection.py
+++ db_connection.py
@@ -2,13 +2,11 @@ import os
 import pandas as pd
 import mysql.connector as mc
 from create_table import get_engine
+from model import Train
 
 
 engine = get_engine()
 
-data_range = pd.read_sql(sql = f"""SELECT * from btc""", con = engine) 
-date15D = data_range.rename(columns={'low': 'Low' , 'date': 'Date', 'high': 'High'}).set_index("Date").tail(15)
-date15D["Dailyrange"] = date15D["High"] - date15D["Low"]
-date15D = date15D["Dailyrange"]
-
-print(date15D)
+model = Train("btc")
+data = model.train_model()
+print(data)
