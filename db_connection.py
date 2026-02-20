0f1d14dce803aa556ba48582f972292512858ed1
mamamo <vaylord.ballesteros@analog.com>
2026-02-20 15:49:20 +0800

    db_changes again

--- db_connection.py
+++ db_connection.py
@@ -2,16 +2,16 @@ import os
 import pandas as pd
 import mysql.connector as mc
 from create_table import get_engine
-<<<<<<< HEAD
+
 import numpy as np
-=======
+
 from model import Train
->>>>>>> 1f6ee157f4445df7878fb274fa5309d11e1b57ce
+
 
 
 engine = get_engine()
 
-<<<<<<< HEAD
+
 data_range = pd.read_sql(sql = f"""SELECT * from btc""", con = engine)
 dataframe =(data_range.rename(columns={"low": "Low", "high": "High", "date": "Date", "close": "Close", "open": "Open"}).set_index("Date").sort_index())
 dataframe['log_return'] = np.log(dataframe['Close'] / dataframe['Close'].shift(1))
@@ -19,8 +19,8 @@ dataframe['vol_30d'] = dataframe['log_return'].rolling(window=30).std() * np.sqr
 
 latest_vol = dataframe['vol_30d'].iloc[-1]
 print("Latest 30-day Volatility:", latest_vol)
-=======
+
 model = Train("btc")
 data = model.train_model()
 print(data)
->>>>>>> 1f6ee157f4445df7878fb274fa5309d11e1b57ce
+
