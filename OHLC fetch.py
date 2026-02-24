acde3666efbd04534e5ff0d23b6f65ba80362647
LordVay <vaylordb@gmail.com>
2026-02-24 20:10:52 +0800

    OHLC Fetch

---
+++ OHLC fetch.py
@@ -0,0 +1,30 @@
+import requests
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+
+headers = {
+    "accept": "application/json",
+    "x-cg-demo-api-key": api_key  
+}
+def get_ohlc(name):
+    url = f"https://api.coingecko.com/api/v3/coins/{name}/ohlc"
+
+    params = {
+        "vs_currency": "usd", 
+        "days": "1"
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+
+    # Convert to DataFrame
+    df = pd.DataFrame(data, columns=["timestamp_ms", "open", "high", "low", "close"])
+    df["date"] = pd.to_datetime(df["timestamp_ms"], unit="ms").dt.date
+    df =(df.rename(columns={"low": "Low", "high": "High", "date": "Date", "close": "Close", "open": "Open"}).set_index("Date").sort_index())
+    df = df.drop("timestamp_ms", axis = 1)
+    df = df.iloc[-1]
+    print(df)
+
+get_ohlc("bitcoin")
--- db_connection.py
+++ db_connection.py
@@ -1,26 +1,7 @@
+from dotenv import load_dotenv
 import os
-import pandas as pd
-import mysql.connector as mc
-from create_table import get_engine
 
-import numpy as np
-
-from model import Train
-
-
-
-engine = get_engine()
-
-
-data_range = pd.read_sql(sql = f"""SELECT * from btc""", con = engine)
-dataframe =(data_range.rename(columns={"low": "Low", "high": "High", "date": "Date", "close": "Close", "open": "Open"}).set_index("Date").sort_index())
-dataframe['log_return'] = np.log(dataframe['Close'] / dataframe['Close'].shift(1))
-dataframe['vol_30d'] = dataframe['log_return'].rolling(window=30).std() * np.sqrt(365)
-
-latest_vol = dataframe['vol_30d'].iloc[-1]
-print("Latest 30-day Volatility:", latest_vol)
-
-model = Train("btc")
-data = model.train_model()
-print(data)
-
+load_dotenv()
+api_key = os.getenv("API_KEY")
+print(api_key)
+    
