cbe9e8c64c6972d76a3ea5f6b1820546c90ed82a
VBallest <vaylordb@gmail.com>
2026-04-09 17:37:31 +0800

    News delete

--- Frontend/Main.py
+++ Frontend/Main.py
@@ -11,22 +11,5 @@ st.caption("Live Prices, Price History, Market Insights and Latest Crypto Headli
 df = get_data()
 df = df.rename(columns= {"name" : "Coin", "symbol" : "Symbol", "current_price" : "Price (USD)", "market_cap" : "Market Cap", "price_change_percentage_24h" : "24 Hours % Change" } )
 st.dataframe(df.style.format({ "Price (USD)": "${:,.2f}", "Market Cap": "${:,.0f}", "24 Hours % Change": "{:.2f}%" }), hide_index= True)
-st.subheader("World's Top 3 CryptoNews as of Today !")
-articles = get_news()
 
-
-for article in articles[0:3]:
-    
-        st.write(article["title"])
-
-        raw_date = article["published_at"].replace("Z", "+00:00")
-        pub_date = datetime.fromisoformat(raw_date)
-        formatted_date = pub_date.strftime("%B %d, %Y %I:%M %p")
-
-        st.caption(formatted_date)
-        st.write(article["description"])
-        button_label = f"Read More: {article['title'][:15]}..."
-        
-        if st.button(button_label, key=article["title"]):
-            st.info(f"Full article: {article['description']}")
         
